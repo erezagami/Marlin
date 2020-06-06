6d699d0ea4def933a1700acd2f90fe45ad0533af
Erez Agami <erez.agami@gmail.com>
2020-06-06 17:48:30 +0300

    fix bltouch prob

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -863,7 +863,7 @@
  *      - normally-open switches to 5V and D32.
  *
  */
-#define Z_MIN_PROBE_PIN PA3 // Pin 32 is the RAMPS default
+#define Z_MIN_PROBE_PIN PB1 // Pin 32 is the RAMPS default
 
 /**
  * Probe Type
